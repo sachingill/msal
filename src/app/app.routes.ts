c1905c2b3d89fa87e8fb8c4144b8797c7d566751
sachingill <sachin.gill07@gmail.com>
2025-03-21 19:11:31 +0530

    Add files via upload

---
+++ src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { FailedComponent } from './failed/failed.component';
+
+export const routes: Routes = [
+  {
+    path: 'login-failed',
+    component: FailedComponent
+  },
+];
